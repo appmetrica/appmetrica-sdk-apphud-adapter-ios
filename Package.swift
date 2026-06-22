6c57e1aa5cbfae2f4606723efe6eb24d7cac6166
alex98 <alex98@yandex-team.com>
2026-06-22 12:28:30 +0300

    refactor Package.swift in ApphudAdapter commit_hash:615f0ea37cadbe89186f0144631679d6e2238f2b

--- Package.swift
+++ Package.swift
@@ -1,79 +1,172 @@
 // swift-tools-version:5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
 
-enum AppMetricaApphudTarget: String {
-    case apphudAdapter = "AppMetricaApphudAdapter"
-    case objcWrapper = "AppMetricaApphudObjCWrapper"
-    case testUtils = "AppMetricaApphudTestUtils"
-    
-    var name: String { rawValue }
-    var testsName: String { rawValue + "Tests" }
-    var path: String { "\(rawValue)/Sources" }
-    var testsPath: String { "\(rawValue)/Tests" }
-    var dependency: Target.Dependency { .target(name: rawValue) }
-}
-
-enum AppMetricaApphudProduct: String, CaseIterable {
-    case apphudAdapter = "AppMetricaApphudAdapter"
-    case objcWrapper = "AppMetricaApphudObjCWrapper"
-    
-    static var allProducts: [Product] { allCases.map { $0.product } }
-    
-    var targets: [AppMetricaApphudTarget] {
-        switch self {
-        case .apphudAdapter: return [.apphudAdapter]
-        case .objcWrapper: return [.objcWrapper]
-        }
-    }
-    
-    var product: Product { .library(name: rawValue, targets: targets.map { $0.name }) }
-}
+// MARK: - Dependencies
 
 let useSpmExternal = false
 
-enum ExternalDependency: String, CaseIterable {
-    case appMetricaCore = "appmetrica-sdk-ios"
-    case apphudSDK = "ApphudSDK"
-    case kiwi = "Kiwi"
-
-    var name: String {
-         switch self {
-            case .appMetricaCore:
-                return useSpmExternal ? "spm-external.AppMetrica" : rawValue
-            default:
-                return useSpmExternal ? ("spm-external." + rawValue) : rawValue
-        }
-    }
-
-    static var allDependecies: [Package.Dependency] { allCases.map { $0.package } }
-
-    var package: Package.Dependency {
-        switch self {
-        case .appMetricaCore: return package(url: "https://github.com/appmetrica/appmetrica-sdk-ios", .upToNextMajor(from: "6.0.0"))
-        case .apphudSDK: return package(url: "https://github.com/apphud/ApphudSDK", .upToNextMajor(from: "3.0.0"))
-        case .kiwi: return package(url: "https://github.com/appmetrica/Kiwi", exact: "3.0.1-spm")
+struct ExternalDependency {
+    let package: String
+    let dependency: Package.Dependency
+    
+    init(url: String, spmExternalId: String, version: VersionSpec) {
+        if useSpmExternal {
+            self.package = "spm-external.\(spmExternalId)"
+            self.dependency = switch version {
+            case .upToNextMajor(from: let from): .package(id: package, .upToNextMajor(from: from))
+            case .exact(let v): .package(id: package, exact: v)
+            }
+        } else {
+            self.package = URL(string: url)!.lastPathComponent
+            self.dependency = switch version {
+            case .upToNextMajor(from: let from): .package(url: url, .upToNextMajor(from: from))
+            case .exact(let v): .package(url: url, exact: v)
+            }
         }
     }
     
-    var dependency: Target.Dependency {
-        switch self {
-        case .appMetricaCore:
-            return .product(name: "AppMetricaCore", package: name)
-        case .apphudSDK, .kiwi:
-            return .product(name: rawValue, package: name)
-        }
-    }
-
-    private func package(url: String, _ version: Range<Version>) -> Package.Dependency {
-        useSpmExternal ? .package(id: name, version) : .package(url: url, version)
-    }
-
-    private func package(url: String, exact: Version) -> Package.Dependency {
-        useSpmExternal ? .package(id: name, exact: exact) : .package(url: url, exact: exact)
+    enum VersionSpec {
+        case upToNextMajor(from: Version)
+        case exact(Version)
     }
 }
+
+enum AppMetrica {
+    private static let dep = ExternalDependency(
+        url: "https://github.com/appmetrica/appmetrica-sdk-ios",
+        spmExternalId: "AppMetrica",
+        version: .upToNextMajor(from: "6.0.0"),
+    )
+ 
+    static let dependency: Package.Dependency = dep.dependency
+    static let core: Target.Dependency = .product(name: "AppMetricaCore", package: dep.package)
+}
+
+enum ApphudSDK {
+    private static let dep = ExternalDependency(
+        url: "https://github.com/apphud/ApphudSDK",
+        spmExternalId: "ApphudSDK",
+        version: .upToNextMajor(from: "3.0.0"),
+    )
+    
+    static let dependency: Package.Dependency = dep.dependency
+    static let apphudSdk: Target.Dependency = .product(name: "ApphudSDK", package: dep.package)
+}
+
+enum Kiwi {
+    private static let dep = ExternalDependency(
+        url: "https://github.com/appmetrica/Kiwi",
+        spmExternalId: "Kiwi",
+        version: .exact("3.0.1-spm"),
+    )
+    
+    static let dependency: Package.Dependency = dep.dependency
+    static let kiwi: Target.Dependency = .product(name: "Kiwi", package: dep.package)
+}
+
+// MARK: - Module
+
+protocol ModuleDependency {
+    var asTargetDependency: Target.Dependency { get }
+}
+
+extension String : ModuleDependency {
+    var asTargetDependency: Target.Dependency { .target(name: self) }
+}
+
+extension Target.Dependency : ModuleDependency {
+    var asTargetDependency: Target.Dependency { self }
+}
+
+struct Module {
+    let name: String
+    let dependencies: [Target.Dependency]
+    let hasTests: Bool
+    let testDependencies: [Target.Dependency]
+    
+    init(name: String, dependencies: [ModuleDependency], hasTests: Bool = true, testDependencies: [ModuleDependency] = []) {
+        self.name = name
+        self.dependencies = dependencies.map(\.asTargetDependency)
+        self.hasTests = hasTests
+        self.testDependencies = testDependencies.map(\.asTargetDependency)
+    }
+    
+    func toTargets() -> [Target] {
+        let headerSearchPaths = [
+            ".",
+            "include",
+            "include/\(name)",
+        ]
+        var targets: [Target] = [
+            .target(
+                name: name,
+                dependencies: dependencies,
+                path: "\(name)/Sources",
+                resources: [.copy("Resources/PrivacyInfo.xcprivacy")],
+                cSettings: headerSearchPaths.map { .headerSearchPath($0) },
+            )
+        ]
+        if hasTests {
+            targets.append(
+                .testTarget(
+                    name: "\(name)Tests",
+                    dependencies: [.target(name: name)] + dependencies + testDependencies,
+                    path: "\(name)/Tests",
+                    cSettings: [
+                        .headerSearchPath("."),
+                    ] + headerSearchPaths.map { .headerSearchPath("../Sources/\($0)") },
+                )
+            )
+        }
+        return targets
+    }
+}
+
+extension Module {
+    static let apphudAdapter = "AppMetricaApphudAdapter"
+    static let objcWrapper = "AppMetricaApphudObjCWrapper"
+    static let testUtils = "AppMetricaApphudTestUtils"
+}
+
+// MARK: - Apphud Adapter Module
+
+let apphudAdapter = Module(
+    name: Module.apphudAdapter,
+    dependencies: [
+        Module.objcWrapper,
+        AppMetrica.core,
+    ],
+    testDependencies: [
+        Kiwi.kiwi,
+    ],
+)
+
+// MARK: - ObjC Wrapper Module
+
+let objcWrapper = Module(
+    name: Module.objcWrapper,
+    dependencies: [
+        AppMetrica.core,
+        ApphudSDK.apphudSdk,
+    ],
+    testDependencies: [
+        Module.testUtils,
+    ],
+)
+
+// MARK: - Test Utils Module
+
+let testUtils = Module(
+    name: Module.testUtils,
+    dependencies: [
+        AppMetrica.core,
+    ],
+    hasTests: false,
+)
+ 
+// MARK: - Package definition
 
 let package = Package(
     name: "AppMetricaApphudAdapter",
@@ -81,106 +174,18 @@ let package = Package(
         .iOS(.v13),
         .tvOS(.v13),
     ],
-    products: AppMetricaApphudProduct.allProducts,
-    dependencies: ExternalDependency.allDependecies,
+    products: [
+        .library(name: "AppMetricaApphudAdapter", targets: [Module.apphudAdapter]),
+        .library(name: "AppMetricaApphudObjCWrapper", targets: [Module.objcWrapper]),
+    ],
+    dependencies: [
+        AppMetrica.dependency,
+        ApphudSDK.dependency,
+        Kiwi.dependency,
+    ],
     targets: [
-        //MARK: - AppMetrica Apphud Adapter -
-        .target(
-            target: .apphudAdapter,
-            dependencies: [.objcWrapper],
-            externalDependencies: [.appMetricaCore]
-        ),
-        //MARK: - AppMetrica Apphud ObjC Wrapper -
-        .target(
-            target: .objcWrapper,
-            dependencies: [],
-            externalDependencies: [.appMetricaCore, .apphudSDK]
-        ),
-        //MARK: - AppMetrica Apphud Test Utils -
-        .target(
-            target: .testUtils,
-            dependencies: [],
-            externalDependencies: [.appMetricaCore]
-        ),
-        //MARK: - Unit tests -
-        .testTarget(
-            target: .apphudAdapter,
-            dependencies: [.apphudAdapter],
-            externalDependencies: [.kiwi]
-        ),
-        .testTarget(
-            target: .objcWrapper,
-            dependencies: [.objcWrapper, .testUtils],
-            externalDependencies: [.appMetricaCore]
-        ),
-    ]
+        apphudAdapter,
+        objcWrapper,
+        testUtils,
+    ].flatMap { $0.toTargets() }
 )
-
-//MARK: - Helpers
-
-extension Target {
-    static func target(target: AppMetricaApphudTarget,
-                       dependencies: [AppMetricaApphudTarget] = [],
-                       externalDependencies: [ExternalDependency] = [],
-                       searchPaths: [String] = [],
-                       includePrivacyManifest: Bool = true) -> Target {
-        var resources: [Resource] = []
-        if includePrivacyManifest {
-            resources.append(.copy("Resources/PrivacyInfo.xcprivacy"))
-        }
-        
-        let resultSearchPath: Set<String> = target.headerPaths.union(searchPaths)
-
-        return .target(
-            name: target.name,
-            dependencies: dependencies.map { $0.dependency } + externalDependencies.map { $0.dependency },
-            path: target.path,
-            resources: resources,
-            cSettings: resultSearchPath.sorted().map { .headerSearchPath($0) }
-        )
-    }
-    
-    static func testTarget(target: AppMetricaApphudTarget,
-                           dependencies: [AppMetricaApphudTarget] = [],
-                           testUtils: [AppMetricaApphudTarget] = [],
-                           externalDependencies: [ExternalDependency] = [],
-                           searchPaths: [String] = [],
-                           resources: [Resource]? = nil) -> Target {
-        
-        let resultSearchPath: Set<String> = target.testsHeaderPaths.union(searchPaths)
-        
-        return .testTarget(
-            name: target.testsName,
-            dependencies: dependencies.map { $0.dependency } + externalDependencies.map { $0.dependency },
-            path: target.testsPath,
-            resources: resources,
-            cSettings: resultSearchPath.sorted().map { .headerSearchPath($0) }
-        )
-    }
-    
-}
-
-//MARK: - Header paths
-
-extension AppMetricaApphudTarget {
-    
-    var headerPaths: Set<String> {
-        let commonPaths: Set<String> = [
-            ".",
-            "include",
-            "include/\(name)"
-        ]
-        
-        return commonPaths
-    }
-    
-    var testsHeaderPaths: Set<String> {
-        let commonPaths: Set<String> = [
-            "."
-        ]
-        
-        let moduleHeaderPaths = headerPaths.map { "../Sources/\($0)" }
-        return commonPaths.union(moduleHeaderPaths)
-    }
-    
-}
